bf15bbd815b80c52c09da6be406db45161df8489
aristides1000 <aristides1000@gmail.com>
2021-04-28 11:57:13 -0400

    some changes in the main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -10,7 +10,7 @@ module Game
       [4,5,6],
       [7,8,9]
       ]
-        
+
       board = "+---+---+---+ \n| #{arr[0][0]} | #{arr[0][1]} | #{arr[0][2]} |\n+---+---+---+ \n| #{arr[1][0]} | #{arr[1][1]} | #{arr[1][2]} |\n+---+---+---+ \n| #{arr[2][0]} | #{arr[2][1]} | #{arr[2][2]} |\n+---+---+---+ "
       puts board
     end
@@ -37,10 +37,42 @@ module Game
     end
   end
   
-  
-  
+  clear =->{
+    system "clear"
+  }
+
   player = Player.new
   Game.start
   
   puts "#{player.name1} is going to play X and #{player.name2} will play 0"
-    
+  sleep(1)
+  puts "Let's start"
+  sleep(2)
+  clear.call
+  Game.start
+  puts "It's #{player.name1}'s turn \n"
+  sleep(1)
+  puts "Please select an available cell from the board"
+  select_player1 = gets.chomp
+  range = (1..9)
+  while select_player1 != range
+    puts "Invalid move, Please select a number between 1 to 9."
+    sleep(1)
+    p select_player1
+    select_player1 = gets.chomp.to_i
+    p select_player1
+  end
+  clear.call
+  Game.start
+  puts "It's #{player.name2}'s turn \n"
+  sleep(1)
+  puts "Please select an available cell from the board"
+  select_player2 = gets.chomp
+  range = (1..9)
+  while select_player2 != range
+    puts "Invalid move, Please select a number between 1 to 9."
+    sleep(1)
+    select_player2 = gets.chomp
+  end
+  clear.call
+  Game.start
