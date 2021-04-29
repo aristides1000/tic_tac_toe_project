8073be4ac173651aa7fa5b609d939b3ecf8de72d
Alexander Morales <alexandermorales.n1@gmail.com>
2021-04-29 10:31:07 -0500

    fix linter errors / remove line length cop

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Layout/LineLength
+
 module Game
   def self.start
     arr = [
@@ -6,9 +8,7 @@ module Game
       [7, 8, 9]
     ]
 
-    grid = " +---+---+---+ \n | #{arr[0][0]} | #{arr[0][1]} | #{arr[0][2]} | \n +---+---+---+ \n | #{arr[1][0]} | #{arr[1][1]} | #{arr[1][2]} | \n +---+---+---+ \n | #{arr[2][0]} | #{arr[2][1]} | #{arr[2][2]} | \n +---+---+---+"
-
-    grid
+    " +---+---+---+ \n | #{arr[0][0]} | #{arr[0][1]} | #{arr[0][2]} | \n +---+---+---+ \n | #{arr[1][0]} | #{arr[1][1]} | #{arr[1][2]} | \n +---+---+---+ \n | #{arr[2][0]} | #{arr[2][1]} | #{arr[2][2]} | \n +---+---+---+"
   end
 end
 
@@ -32,3 +32,5 @@ class Rules
     @select_player2 = select_player2
   end
 end
+
+# rubocop: enable Layout/LineLength
